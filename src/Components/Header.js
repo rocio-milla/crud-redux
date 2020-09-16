f8e8553c64c48503a627203dc16be11074a9c29a
rocio-milla <rocio.milla@moxit.net>
2020-09-16 14:10:02 -0300

    Created base

---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary justify-content-between">
+            <div className="container">
+                <h1>CRUD - React, Redux, REST API & Axios</h1>
+            </div>
+            <a href="/productos/nuevo" className="btn btn-danger new-post d-block d-md-inline-block">Add item &#43;</a>
+        </nav>
+    );
+}
+
+export default Header;
