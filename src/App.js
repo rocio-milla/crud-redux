5d7a363e0cf3d17c4431abe3e3bb0709b3767a53
rocio-milla <rocio.milla@moxit.net>
2020-09-17 10:32:10 -0300

    Added redux

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import EditItem from './Components/EditItem';
 import Header from './Components/Header';
 import Items from './Components/Items';
 import NewItem from './Components/NewItem';
+import store from './store';
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <div className="container">
-        <Switch>
-          <Route exact path="/" component={Items} />
-          <Route exact path="/items/new" component={NewItem} />
-          <Route exact path="/items/edit/:id" component={EditItem} />
-        </Switch>
-      </div>
+      <Provider store={store}>
+        <Header />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Items} />
+            <Route exact path="/items/new" component={NewItem} />
+            <Route exact path="/items/edit/:id" component={EditItem} />
+          </Switch>
+        </div>
+      </Provider>
     </BrowserRouter>
   );
 }
---
+++ src/reducers/itemsReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+    items: [],
+    error: null,
+    loading: false
+}
+
+export default function (state = initialState, action) {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
